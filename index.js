c2c03eebc4227e4f98efe9d9d9a506a36dd9ccb7
rahul0504 <32923485+rahul0504@users.noreply.github.com>
2017-10-26 15:31:59 +0530

    fixing

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /**
+ * Created by Rahul on 23/8/17.
+ */
+/**
  * Automatic Image resize, reduce with AWS Lambda
  * Lambda main handler
  *
@@ -16,7 +19,6 @@ const path           = require("path");
 
 // Lambda Handler
 exports.handler = (event, context, callback) => {
-console.log("----------2--------")
     var eventRecord = eventParser(event);
     if (eventRecord) {
         process(eventRecord, callback);
@@ -28,7 +30,7 @@ console.log("----------2--------")
 };
 
 function process(s3Object, callback) {
-    const configPath = path.resolve(__dirname, "config.json");
+    const configPath = path.resolve(__dirname, "./config.json");
     const fileSystem = new S3FileSystem();
     const processor  = new ImageProcessor(fileSystem, s3Object);
     const config     = new Config(
@@ -36,24 +38,24 @@ function process(s3Object, callback) {
     );
 
     processor.run(config)
-    .then((processedImages) => {
-        const message = "OK, " + processedImages + " images were processed.";
-        console.log(message);
-        callback(null, message);
-        return;
-    })
-    .catch((messages) => {
-        if ( messages === "Object was already processed." ) {
-            console.log("Image already processed");
-            callback(null, "Image already processed");
+        .then((processedImages) => {
+        console.log("-----------processed image-------",processedImages)
+            callback(null, processedImages);
             return;
-        } else if ( messages === "Empty file or directory." ) {
-            console.log( "Image file is broken or it's a folder" );
-            callback( null, "Image file is broken or it's a folder" );
-            return;
-        } else {
-            callback("Error processing " + s3Object.object.key + ": " + messages);
-            return;
-        }
-    });
+        })
+        .catch((messages) => {
+            if ( messages === "Object was already processed." ) {
+                console.log("Image already processed");
+                callback("Image already processed",null);
+                return;
+            } else if ( messages === "Empty file or directory." ) {
+                console.log( "Image file is broken or it's a folder" );
+                callback("Image file is broken or it's a folder",null);
+                return;
+            } else {
+                console.log("Error message :: ",messages)
+                callback(messages,null);
+                return;
+            }
+        });
 }
